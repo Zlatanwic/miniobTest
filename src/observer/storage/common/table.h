e5ec9120578c6a399bffccfc30fe0202e831ad25
Zlatanwic <2353113@tongji.edu.cn>
2025-05-16 16:44:35 +0800

    4

--- src/observer/storage/common/table.h
+++ src/observer/storage/common/table.h
@@ -67,7 +67,7 @@ public:
       void (*record_reader)(const char *data, void *context));
 
   RC create_index(Trx *trx, const char *index_name, const char *attribute_name);
-
+  RC destroy(const char* dir);
   RC get_record_scanner(RecordFileScanner &scanner);
 
   RecordFileHandler *record_handler() const

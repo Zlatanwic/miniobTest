9116da0e88ca4521ce9f0d2679805c1296369922
Zlatanwic <2353113@tongji.edu.cn>
2025-05-16 17:19:29 +0800

    7

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -926,38 +926,33 @@ RC Table::sync()
   return rc;
 }
 
-RC Table::destroy(const char* dir) {
-    RC rc = sync();//刷新所有脏页
-
-    if(rc != RC::SUCCESS) return rc;
-
-    std::string path = table_meta_file(dir, name());
-    if(unlink(path.c_str()) != 0) {
-        LOG_ERROR("Failed to remove meta file=%s, errno=%d", path.c_str(), errno);
-        return RC::GENERIC_ERROR;
+RC Table::destroy(const char *dir)
+{
+    // 删除表数据文件
+    std::string data_file = std::string(dir) + "/" + name() + ".tbl";
+    int rc = remove(data_file.c_str());
+    if (rc != 0) {
+        LOG_WARN("Failed to delete table data file: %s", data_file.c_str());
+        return RC::IOERR_DELETE;
     }
 
-    std::string data_file = std::string(dir) + "/" + name() + TABLE_DATA_SUFFIX;
-    if(unlink(data_file.c_str()) != 0) { // 删除描述表元数据的文件
-        LOG_ERROR("Failed to remove data file=%s, errno=%d", data_file.c_str(), errno);
-        return RC::GENERIC_ERROR;
+    // 删除表元数据文件
+    std::string meta_file = std::string(dir) + "/" + name() + ".meta";
+    rc = remove(meta_file.c_str());
+    if (rc != 0) {
+        LOG_WARN("Failed to delete table meta file: %s", meta_file.c_str());
+        return RC::IOERR_DELETE;
     }
 
-    std::string text_data_file = std::string(dir) + "/" + name() + TABLE_DATA_SUFFIX;
-    if(unlink(text_data_file.c_str()) != 0) { // 删除表实现text字段的数据文件（后续实现了text case时需要考虑，最开始可以不考虑这个逻辑）
-        LOG_ERROR("Failed to remove text data file=%s, errno=%d", text_data_file.c_str(), errno);
-        return RC::GENERIC_ERROR;
-    }
-
-    const int index_num = table_meta_.index_num();
-    for (int i = 0; i < index_num; i++) {  // 清理所有的索引相关文件数据与索引元数据
-        ((BplusTreeIndex*)indexes_[i])->close();
-        const IndexMeta* index_meta = table_meta_.index(i);
-        std::string index_file = make_index_data_file(dir, name(), index_meta->name());
-        if(unlink(index_file.c_str()) != 0) {
-            LOG_ERROR("Failed to remove index file=%s, errno=%d", index_file.c_str(), errno);
-            return RC::GENERIC_ERROR;
+    // 删除所有索引文件
+    for (IndexMeta *index_meta : table_meta_.indexes()) {
+        std::string index_file = std::string(dir) + "/" + name() + "." + index_meta->name() + ".idx";
+        rc = remove(index_file.c_str());
+        if (rc != 0) {
+            LOG_WARN("Failed to delete index file: %s", index_file.c_str());
+            return RC::IOERR_DELETE;
         }
     }
+
     return RC::SUCCESS;
 }
